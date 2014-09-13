69aa9d3e8c4e750d1ead1fdc421acb778ca11768
Peter Varshavsky <pvarsh@gmail.com>
2014-09-13 18:37:02 -0400

    Problems 1-4 with reusing code

--- Assignment-1/code/problem1.py
+++ Assignment-1/code/problem1.py
@@ -36,42 +36,58 @@ def complaintRange(filename):
 
 def complaintTypes(filename):
 
-    typesDict = {}
-    
-    with open(filename) as f:
-        csvf = csv.reader(f, delimiter = ',')
-        csvf.next() # skip header
-        for line in csvf:
-            if line[5] in typesDict.keys():
-                typesDict[line[5]] += 1
-            else:
-                typesDict[line[5]] = 1
-    for item in typesDict.keys():
-        print item + " with " + str(typesDict[item]) + " complaints"
+    typesDict = buildComplaintDict(filename)
+
+    print "\nPROBLEM 2: "
+    printComplaintCounts([(x, typesDict[x]) for x in typesDict])
         
 def complaintTypesSorted(filename):
-    typesDict = {}
-    
-    with open(filename) as f:
-        csvf = csv.reader(f, delimiter = ',')
-        csvf.next() # skip header
-        for line in csvf:
-            if line[5] in typesDict.keys():
-                typesDict[line[5]] += 1
-            else:
-                typesDict[line[5]] = 1
+    typesDict = buildComplaintDict(filename)
 
     typesList = [(x, typesDict[x]) for x in typesDict]
     typesList = sorted(typesList, key = lambda x: x[0])
     typesList = sorted(typesList, key = lambda x: x[1], reverse = True)
- 
-    print typesList
 
-    
+    return typesList
+
+def buildComplaintDict(filename):
+    typesDict = {}
+
+    with open(filename) as f:
+        csvf = csv.reader(f, delimiter = ',')
+        csvf.next() # skip header
+        for line in csvf:
+            if line[5] in typesDict.keys():
+                typesDict[line[5]] += 1
+            else:
+                typesDict[line[5]] = 1
+    return typesDict
+
+def topComplaints(filename, n):
+    typesList = complaintTypesSorted(filename)
+    if n >= 0:
+        printComplaintCount(typesList[0:n])
+    else:
+        print "Must request a non-negative number of services"
+
+def printComplaintCounts(complaintList):
+    for item in complaintList:
+        print item[0] + ' with ' + str(item[1]) + ' complaints'
+            
+
+def problem3(filename):
+    print "\nPROBLEM 3:"
+    printComplaintCounts(complaintTypesSorted(filename))
+
+def problem4(filename, n):
+    print "\nPROBLEM 4:"
+    printComplaintCounts(complaintTypesSorted(filename)[0:n])
         
-    
+
 #complaintRange(filename)
-#complaintTypes(filename)
+complaintTypes(filename)
 complaintTypesSorted(filename)
+problem3(filename)
+problem4(filename, 2)
 
 
