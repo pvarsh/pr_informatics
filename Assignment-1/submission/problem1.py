5fd91130543fe4db0d07e3bde199cee92e89f6be
Peter Varshavsky <pvarsh@gmail.com>
2014-09-14 21:13:58 -0400

    Problem 1 finished

--- Assignment-1/submission/problem1.py
+++ Assignment-1/submission/problem1.py
@@ -11,5 +11,41 @@ import time
 from time import strptime
 from time import strftime
 
-for arg in sys.argv:
-    print arg
+
+def main(argv):
+
+    if len(argv) == 1:
+        complaintCounter, time_min, time_max = readFileProblem1(argv[0])
+        print_problem1(complaintCounter, time_min, time_max)
+    else:
+        print "Invalid number of arguments. Call format: 'python problem1.py data_file.csv'"
+    
+    #print str(complaintCounter) + " complaints between " + strftime('%m/%d/%Y %H:%M:%S', time_min) + " and " + strftime('%m/%d/%Y %H:%M:%S', time_max)
+
+def readFileProblem1(filename):
+
+    # initializing min and max times
+    time_max = strptime('00', '%y') 
+    time_min = strptime('9999', '%Y')
+    complaintCounter = 0
+    
+    with open(filename) as f:
+        csvf = csv.reader(f, delimiter=',')
+        csvf.next() # skip header
+        for line in csvf:
+            complaintCounter += 1
+            lineTime = strptime(line[1], '%m/%d/%Y %I:%M:%S %p')
+            if lineTime < time_min:
+                time_min = lineTime
+            if lineTime > time_max:
+                time_max = lineTime
+    return complaintCounter, time_min, time_max
+    
+def print_problem1(complaintCounter, time_min, time_max):
+    print str(complaintCounter) + " complaints between " + strftime('%m/%d/%Y %H:%M:%S', time_min) + " and " + strftime('%m/%d/%Y %H:%M:%S', time_max)
+    
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
