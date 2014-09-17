4fb402eb92d54ea57a636e4604088e55c9069772
Peter Varshavsky <pvarsh@gmail.com>
2014-09-17 11:08:32 -0400

    Finished update_all()

--- assignment-2/code/assignment_2.py
+++ assignment-2/code/assignment_2.py
@@ -27,11 +27,12 @@ def readCommandFile(db, file):
             if command == 'dump':
                 print 'output file name?'
             if command == 'update_all':
-                print "\n\n"
-                update_all(db, values)
+                try:
+                    update_all(db, values)
+                except ValueError as detail:
+                    print "Insert warning: ", detail
                 
             
-
 def clear(db):
     for i, column in enumerate(db):
         db[i] = [column[0]]
@@ -83,13 +84,27 @@ def update_all(db, values):
     #               update_field_name
     #               update_field_value
 
-    qf_index = fieldNameIndex(db, values[0])
-    uf_index = fieldNameIndex(db, values[2])
     
+    qf_index = fieldNameIndex(db, values[0]) # query field index
+    uf_index = fieldNameIndex(db, values[2]) # update field index
+
+    # converting strings to numbers for id, salary range, # positions open
+    if qf_index in [0, 2]:
+        print "AAA qf_index: ", qf_index
+        values[1] = int(values[1])
+    if qf_index in [5, 6]:
+        values[1] = float(values[1])
+    if uf_index in [0, 2]:
+        values[3] = int(values[3])
+    if uf_index in [5, 6]:
+        values[3] = float(values[3])
+        
     update_rows = [i for i, x in enumerate(db[qf_index]) if x == values[1]]
+    if len(update_rows) == 0:
+        raise ValueError("Query field value does not exist in the database")
     
     print "Update where column ", qf_index, " has value ", values[1]
-    print update_rows
+    print "Rows to be updated: ", update_rows
 
     for row in update_rows:
         db[uf_index][row] = values[3]
@@ -109,7 +124,6 @@ def test(csvFile, file):
     db = readHeader(csvFile)
     clear(db)
  
-
     readCommandFile(db, file)
     prettyPrint(db)
 
@@ -126,17 +140,40 @@ def prettyPrint(db, maxrows = 200, nchar = 10):
         outstring = outstring + '\n'
     print outstring
         
+def makeTestCase1(fileOut, nrows = 5):
 
+    strOut = 'clear\n'
+    for row in range(nrows):
+        strOut = strOut + 'insert'
+        strOut = strOut + '|' + str(row) + str(row)
+        for col in range(1,15):
+            if col in [2, 5, 6]:
+                strOut = strOut + '|' + '33'
+            else: 
+                strOut = strOut + '|' + 'r' + str(row) + 'c' + str(col)
+        strOut= strOut + '\n'
+    print strOut
+
+    with open(fileOut, 'w') as f:
+        f.write(strOut)
+    
             
         
     
 #[[id], [numPos], [busTitle], [civServTitle]]
 
+
+
 path = "/Users/petervarshavsky/Documents/Git_NYU/Principles-of-informatics/Pr-informatics/assignment-2/data/"
-file1 = path + "sample_data_problem_2.txt"
+file1 = path + "sample_data_problem_1.txt"
+file2 = path + "sample_data_problem_2.txt"
+tc1 = path + "test_case_1.txt"
 csvFile = path + "NYC_Jobs_sample.csv"
+
+#makeTestCase1(tc1)
+
 #readCommands(file1)
 #print readHeader(csvFile)
-db = test(csvFile, file1)
+db = test(csvFile, file2)
 
 
