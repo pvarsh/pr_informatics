eb5a70ab85b305f4592ec4fc422bd9c9c8b63a7b
Peter Varshavsky <pvarsh@gmail.com>
2014-10-29 16:54:11 -0400

    Working on 1.1

--- assignment-7/assignment_7.py
+++ assignment-7/assignment_7.py
@@ -19,3 +19,16 @@ pr1_1 = grouped.count()
 
 ### Problem 1
 pr1_agencies = ['NYPD','DOT','DOB','TLC','DPR']
+
+fig, ax1 = plt.subplots()
+centers = range(len(pr1_agencies))
+plt.bar(centers, pr1_1[pr1_agencies])
+ax1.set_title('Complaint volumes by agency')
+ax1.set_xlabel('Agency')
+ax1.set_ylabel('Volume')
+xlim = ax1.get_xlim()
+xlim = (xlim[0]-0.25, xlim[1])
+ax1.set_xlim(xlim)
+ax1.set_xticks([x+0.75/2 for x in centers])
+ax1.set_xticklabels(pr1_agencies)
+plt.show()
